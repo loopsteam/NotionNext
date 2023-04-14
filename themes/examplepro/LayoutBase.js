9fd6fc82fe9f9b11e5456c7db94eb76b9b929e18
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 04:08:45 +0800

    Update LayoutBase.js

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -8,8 +8,7 @@ import BLOG from '@/blog.config'
 import { useGlobal } from '@/lib/global'
 
 /**
- * 基础布局 采用左右两侧布局，左侧占四分之一，右侧占四分之三，左侧由上到下依次为顶栏logo，菜单，左右使用粗线隔开，菜单使用竖导航栏
- *
+ * 基础布局 采用顶部导航栏和主体布局，主体部分居中显示，左右留有一定的空白
  * @returns {JSX.Element}
  * @constructor
  */
@@ -17,41 +16,32 @@ const LayoutBase = props => {
   const { children, meta } = props
   const { onLoading } = useGlobal()
 
-  const LoadingCover = (
-    <div
-      id="cover-loading"
-      className={`${onLoading ? 'z-50 opacity-50' : '-z-10 opacity-0'} pointer-events-none transition-all duration-300`}
-    >
-      <div className="w-full h-screen flex justify-center items-center">
-        <i className="fa-solid fa-spinner text-2xl text-black dark:text-white animate-spin"> </i>
-      </div>
+  const LoadingCover = <div id='cover-loading' className={`${onLoading ? 'z-50 opacity-50' : '-z-10 opacity-0'} pointer-events-none transition-all duration-300`}>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <i className="fa-solid fa-spinner text-2xl text-black dark:text-white animate-spin">  </i>
     </div>
-  )
+  </div>
 
   return (
-    <div id="theme-example" className="dark:text-gray-300 bg-white dark:bg-black">
+    <div id='theme-example' className='dark:text-gray-300 bg-white dark:bg-black'>
       <CommonHead meta={meta} />
+      {/* 顶栏LOGO */}
+      <Header {...props} />
 
-      {/* 左侧 */}
-      <div className="flex flex-col w-1/4 h-screen border-r-4 border-gray-400 dark:border-gray-600">
-        <Header {...props} />
-        <div className="flex-1 overflow-auto mt-4">
-          <Nav {...props} />
-        </div>
+      {/* 菜单 */}
+      <div className="flex flex-col md:flex-row md:justify-center md:items-center border-b-2 border-gray-200 dark:border-gray-800">
+        <Nav {...props} />
+        <div className="md:mx-4 w-full md:w-auto md:block border-l-2 border-gray-200 dark:border-gray-800"></div>
       </div>
 
-      {/* 右侧 */}
-      <div className="flex-1 px-8 py-8 overflow-auto">
-        {/* 菜单和主体之间的粗线 */}
-        <div className="border-l-4 border-gray-400 dark:border-gray-600 h-full"></div>
-        <div className="max-w-3xl mx-auto mt-4">
-          {onLoading ? LoadingCover : children}
-        </div>
+      {/* 主体 */}
+      <div className="w-full max-w-3xl px-4 py-8 mx-auto">
+        {onLoading ? LoadingCover : children}
       </div>
 
       <Footer {...props} />
 
-      <div className="fixed right-4 bottom-4 z-10">
+      <div className='fixed right-4 bottom-4 z-10'>
         <JumpToTopButton />
       </div>
     </div>
