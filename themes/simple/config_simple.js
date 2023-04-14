e165aab840440362859cf0ff66e35185fab01d5b
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-14 16:50:53 +0800

    Update config_simple.js

--- themes/simple/config_simple.js
+++ themes/simple/config_simple.js
@@ -1,7 +1,7 @@
 const CONFIG_SIMPLE = {
 
   LOGO_IMG: '/Logo.webp',
-  TOP_BAR: true, // 显示顶栏
+  TOP_BAR: false, // 显示顶栏
   TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
   LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>编程爱好者<br/>/互联网从业者<br/>/知识分享博主</div>',
 
@@ -9,7 +9,7 @@ const CONFIG_SIMPLE = {
 
   // 菜单配置
   MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
+  MENU_TAG: flase, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true // 显示搜索
 }
