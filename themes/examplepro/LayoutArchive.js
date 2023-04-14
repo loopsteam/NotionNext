d6fa309cdd36509e4bb54de56140c0a748afbb00
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 03:15:40 +0800

    修改归档按照时间顺序排序

--- themes/examplepro/LayoutArchive.js
+++ themes/examplepro/LayoutArchive.js
@@ -25,28 +25,30 @@ export const LayoutArchive = props => {
               - {year} -
             </div>
             <ul className="pl-6 space-y-2">
-              {groupByYear[year].map(post => (
-                <li
-                  key={post.id}
-                  className="border-l-2 p-1 text-sm md:text-base items-center hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
-                >
-                  <div>
-                    <span className="text-gray-400">
-                      {post.date.start_date}
-                    </span>
-                    <span className="mx-2">
-                      ✨
-                    </span>
-                    <Link
-                      href={`${BLOG.SUB_PATH}/${post.slug}`}
-                      passHref
-                      className="dark:text-gray-400 dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600"
-                    >
-                      {post.title}
-                    </Link>
-                  </div>
-                </li>
-              ))}
+              {groupByYear[year]
+                .sort((a, b) => new Date(b.date.start_date) - new Date(a.date.start_date))
+                .map(post => (
+                  <li
+                    key={post.id}
+                    className="border-l-2 p-1 text-sm md:text-base items-center hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
+                  >
+                    <div>
+                      <span className="text-gray-400">
+                        {post.date.start_date}
+                      </span>
+                      <span className="mx-2">
+                        ✨
+                      </span>
+                      <Link
+                        href={`${BLOG.SUB_PATH}/${post.slug}`}
+                        passHref
+                        className="dark:text-gray-400 dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600"
+                      >
+                        {post.title}
+                      </Link>
+                    </div>
+                  </li>
+                ))}
             </ul>
           </div>
         ))}
