8918bdcd2ca7c8f3598e0d1c724165c6309379a8
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 06:49:44 +0800

    修改主题
    1.居左
    2.给Conner Stars⭐一个动态下划线
    3.在Conner Stars⭐有副标题"哈喽呀"，字体为lg

--- themes/examplepro/components/Header.js
+++ themes/examplepro/components/Header.js
@@ -12,9 +12,13 @@ export const Header = (props) => {
             <div className="container mx-auto max-w-4xl md:flex justify-between items-center">
                 <Link
                     href='/'
-                    className="py-6 w-full text-center md:text-left md:w-auto no-underline flex justify-center items-center">
+                    className="py-6 w-full text-left md:text-left md:w-auto no-underline flex justify-center items-center">
                     
-                    <span className="text-3xl">Conner Stars⭐</span>
+                    <span className="text-3xl relative">
+                      Conner Stars⭐
+                      <span className="absolute bottom-0 left-0 w-full h-1 bg-white transform transition-transform origin-left hover:scale-x-100"></span>
+                    </span>
+                    <span className="text-lg font-medium ml-2">哈喽呀</span>
                     
                 </Link>
                 <div className="w-full md:w-auto text-center md:text-right">
