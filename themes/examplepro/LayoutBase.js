e5641074490fb1d5914bcd8dda478899938b9cab
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 03:39:34 +0800

    更改布局
    移除侧边栏，然后将侧边栏的位置放置导航栏，导航栏为竖导航栏，导航栏与主体用竖线分隔开

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -3,12 +3,12 @@ import React from 'react'
 import { Header } from './components/Header'
 import { Nav } from './components/Nav'
 import { Footer } from './components/Footer'
-import { SideBar } from './components/SideBar'
 import JumpToTopButton from './components/JumpToTopButton'
 import BLOG from '@/blog.config'
 import { useGlobal } from '@/lib/global'
+
 /**
- * 基础布局 采用左右两侧布局，移动端使用顶部导航栏
+ * 基础布局 采用顶部导航栏和主体布局，主体部分居中显示，左右留有一定的空白
 
  * @returns {JSX.Element}
  * @constructor
@@ -18,39 +18,34 @@ const LayoutBase = props => {
   const { onLoading } = useGlobal()
 
   const LoadingCover = <div id='cover-loading' className={`${onLoading ? 'z-50 opacity-50' : '-z-10 opacity-0'} pointer-events-none transition-all duration-300`}>
-        <div className='w-full h-screen flex justify-center items-center'>
-            <i className="fa-solid fa-spinner text-2xl text-black dark:text-white animate-spin">  </i>
-        </div>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <i className="fa-solid fa-spinner text-2xl text-black dark:text-white animate-spin">  </i>
     </div>
+  </div>
 
   return (
-        <div id='theme-example' className='dark:text-gray-300  bg-white dark:bg-black'>
-            <CommonHead meta={meta} />
-            {/* 顶栏LOGO */}
-            <Header {...props} />
+    <div id='theme-example' className='dark:text-gray-300 bg-white dark:bg-black'>
+      <CommonHead meta={meta} />
+      {/* 顶栏LOGO */}
+      <Header {...props} />
 
-            {/* 菜单 */}
-            <Nav {...props} />
+      {/* 菜单 */}
+      <div className="flex flex-col md:flex-row md:justify-center md:items-center border-b-2 border-gray-200 dark:border-gray-800">
+        <Nav {...props} />
+        <div className="md:mx-4 w-full md:w-auto md:block border-l-2 border-gray-200 dark:border-gray-800"></div>
+      </div>
 
-            {/* 主体 */}
-            <div id='container-inner' className="w-full relative z-10">
+      {/* 主体 */}
+      <div className="w-full max-w-3xl px-4 py-8 mx-auto">
+        {onLoading ? LoadingCover : children}
+      </div>
 
-                <div className={(BLOG.LAYOUT_SIDEBAR_REVERSE ? 'flex-row-reverse' : '') + 'relative container mx-auto justify-center md:flex items-start py-8 px-2'}>
+      <Footer {...props} />
 
-                    <div className='w-full max-w-3xl xl:px-14 lg:px-4 '> {onLoading ? LoadingCover : children}</div>
-
-                    <SideBar {...props} />
-
-                </div>
-
-            </div>
-
-            <Footer {...props} />
-
-            <div className='fixed right-4 bottom-4 z-10'>
-                <JumpToTopButton />
-            </div>
-        </div>
+      <div className='fixed right-4 bottom-4 z-10'>
+        <JumpToTopButton />
+      </div>
+    </div>
   )
 }
 
