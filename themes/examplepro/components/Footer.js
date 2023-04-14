758893fa92bbc08ae5343fcd7b3b7e0569728dde
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 00:47:21 +0800

    修改暗夜模式按钮位置，修改页脚位置

--- themes/examplepro/components/Footer.js
+++ themes/examplepro/components/Footer.js
@@ -11,20 +11,26 @@ export const Footer = (props) => {
     return currentYear
   })()
 
-  return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
-        <DarkModeButton className='text-center pt-4'/>
-
-        <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {BLOG.AUTHOR}. All rights reserved.</div>
-            <div className="md:p-0 text-center md:text-right text-xs">
-                {/* 右侧链接 */}
-                {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
-                {BLOG.BEI_AN && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{BLOG.BEI_AN} </a>)}
-                <span className='dark:text-gray-200 no-underline ml-4'>
-                    Powered by
-                    <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> NotionNext {BLOG.VERSION}  </a>
-                </span>
-            </div>
+  return (
+    <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray fixed bottom-0">
+      <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
+        <div className='text-center'>
+          &copy;{`${copyrightDate}`}
+          {BLOG.AUTHOR}. All rights reserved.
         </div>
+        <div className="md:p-0 text-center md:text-right text-xs flex items-center">
+          {/* DarkModeButton 放在这里 */}
+          <DarkModeButton className='text-center pt-4 ml-auto' />
+          {BLOG.BEI_AN && (
+            <a
+              href="https://beian.miit.gov.cn/"
+              className="text-black dark:text-gray-200 no-underline hover:underline ml-4"
+            >
+              {BLOG.BEI_AN}
+            </a>
+          )}
+        </div>
+      </div>
     </footer>
+  )
 }
