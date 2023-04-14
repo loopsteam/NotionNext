531c1ed703bccaf8eebcaf8d335369692496eb29
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 00:34:26 +0800

    Update LayoutBase.js

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -35,8 +35,6 @@ const LayoutBase = props => {
             {/* 主体 */}
             <div id='container-inner' className="w-full relative z-10">
 
-                <Title {...props} />
-
                 <div className={(BLOG.LAYOUT_SIDEBAR_REVERSE ? 'flex-row-reverse' : '') + 'relative container mx-auto justify-center md:flex items-start py-8 px-2'}>
 
                     <div className='w-full max-w-3xl xl:px-14 lg:px-4 '> {onLoading ? LoadingCover : children}</div>
