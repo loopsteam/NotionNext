a48eb267d4c4713a7cf6be0c0be6235eca66de08
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 04:45:17 +0800

    更改布局

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -1,5 +1,5 @@
 import CommonHead from '@/components/CommonHead'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Header } from './components/Header'
 import { Nav } from './components/Nav'
 import { Footer } from './components/Footer'
@@ -15,7 +15,6 @@ import { useGlobal } from '@/lib/global'
 const LayoutBase = props => {
   const { children, meta } = props
   const { onLoading } = useGlobal()
-  const [navFixed, setNavFixed] = useState(false)
 
   const LoadingCover = <div id='cover-loading' className={`${onLoading ? 'z-50 opacity-50' : '-z-10 opacity-0'} pointer-events-none transition-all duration-300`}>
     <div className='w-full h-screen flex justify-center items-center'>
@@ -23,32 +22,20 @@ const LayoutBase = props => {
     </div>
   </div>
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setNavFixed(true)
-      } else {
-        setNavFixed(false)
-      }
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
-
   return (
-    <div id='theme-example' className='dark:text-gray-300 bg-white dark:bg-black'>
+    <div id='theme-example' className='dark:text-gray-300 bg-white dark:bg-black flex flex-col min-h-screen '>
       <CommonHead meta={meta} />
       {/* 顶栏LOGO */}
       <Header {...props} />
 
       {/* 菜单 */}
-      <div className={`flex flex-col md:flex-row md:justify-center md:items-center ${navFixed ? 'fixed top-0 z-50' : 'relative'} bg-white dark:bg-black transition-all duration-300 ${navFixed ? 'shadow-md' : ''}`}>
+      <div className="flex flex-col md:flex-row md:justify-center md:items-center border-b-2 border-gray-200 dark:border-gray-800">
         <Nav {...props} />
-        <div className="md:mx-4 w-full md:w-auto md:block"></div>
+        <div className="md:mx-4 w-full md:w-auto md:block border-l-2 border-gray-200 dark:border-gray-800"></div>
       </div>
 
       {/* 主体 */}
-      <div className="w-full max-w-3xl px-4 py-8 mx-auto">
+      <div className="flex-grow">
         {onLoading ? LoadingCover : children}
       </div>
 
