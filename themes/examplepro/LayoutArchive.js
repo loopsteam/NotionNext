7a41642b182ddd509c981e140ffb6af0a38515fe
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 01:34:32 +0800

    更改归档格式

--- themes/examplepro/LayoutArchive.js
+++ themes/examplepro/LayoutArchive.js
@@ -10,19 +10,18 @@ export const LayoutArchive = props => {
       <div className="mb-10 pb-20 md:py-12 p-3  min-h-screen w-full">
         {Object.keys(archivePosts).map((archiveTitle, index) => (
           <div key={archiveTitle}>
-            <div id={archiveTitle} className={`pt-${index === 0 ? 0 : 8} pb-4 text-sm dark:text-gray-300 border-t-2 border-gray-300`} >
-              {archiveTitle}
-            </div>
+            <div className={`py-4 border-t-2 border-b-2 border-gray-300`} />
+
             <ul>
               {archivePosts[archiveTitle].map(post => (
                 <li
                   key={post.id}
                   className="border-l-2 p-1 text-sm md:text-base items-center  hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
                 >
-                  <div id={post?.date?.start_date} className="flex justify-between items-center">
+                  <div className="relative" id={post?.date?.start_date}>
                     <span className="text-gray-400">
                       {post.date?.start_date}
-                    </span>{' '}
+                    </span>
                     <span className="mx-2">
                       ✨
                     </span>
@@ -30,8 +29,12 @@ export const LayoutArchive = props => {
                       href={`${BLOG.SUB_PATH}/${post.slug}`}
                       passHref
                       className="dark:text-gray-400  dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600">
-                      {post.title}
+                      <a className="text-left">
+                        {post.title}
+                      </a>
                     </Link>
+                    <div className="absolute top-1/2 left-0 w-full border-t-2 border-gray-300"></div>
+                    <div className="absolute bottom-1/2 left-0 w-full border-t-2 border-gray-300"></div>
                   </div>
                 </li>
               ))}
