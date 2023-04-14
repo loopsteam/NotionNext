539829561950621e310a783fba483408c54929e6
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 03:08:16 +0800

    修改归档

--- themes/examplepro/LayoutArchive.js
+++ themes/examplepro/LayoutArchive.js
@@ -14,31 +14,33 @@ export const LayoutArchive = props => {
     return acc
   }, {})
 
+  const years = Object.keys(groupByYear).sort((a, b) => b - a)
+
   return (
     <LayoutBase {...props}>
-      <div className="mb-10 pb-20 md:py-12 p-3 min-h-screen w-full">
-        {Object.entries(groupByYear).map(([year, posts]) => (
-          <div key={year}>
-            <div className="pt-16 pb-4 text-lg font-bold text-center dark:text-gray-300">
+      <div className="my-16 pb-20 md:py-12 px-3 min-h-screen w-full">
+        {years.map(year => (
+          <div key={year} className="mb-8">
+            <div className="pb-4 text-lg font-bold text-center dark:text-gray-300">
               - {year} -
             </div>
-            <ul>
-              {posts.map(post => (
+            <ul className="pl-6 space-y-2">
+              {groupByYear[year].map(post => (
                 <li
                   key={post.id}
-                  className="border-l-2 p-1 text-sm md:text-base items-center  hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
+                  className="border-l-2 p-1 text-sm md:text-base items-center hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
                 >
                   <div>
                     <span className="text-gray-400">
                       {post.date.start_date}
                     </span>
                     <span className="mx-2">
-                    ✨
+                      ✨
                     </span>
                     <Link
                       href={`${BLOG.SUB_PATH}/${post.slug}`}
                       passHref
-                      className="dark:text-gray-400  dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600"
+                      className="dark:text-gray-400 dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600"
                     >
                       {post.title}
                     </Link>
