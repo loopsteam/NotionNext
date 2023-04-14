7eee2a4b1e8d9c64b2c9978b3be5f44fdd0007cc
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 06:44:05 +0800

    修改菜单

--- themes/examplepro/components/Nav.js
+++ themes/examplepro/components/Nav.js
@@ -13,10 +13,9 @@ export const Nav = (props) => {
   const { locale } = useGlobal()
 
   let links = [
-    {  name: locale.NAV.SEARCH, to: '/search', show: CONFIG_EXAMPLE.MENU_SEARCH },
-    {  name: locale.NAV.ARCHIVE, to: '/archive', show: CONFIG_EXAMPLE.MENU_ARCHIVE },
-    {  name: locale.COMMON.CATEGORY, to: '/category', show: CONFIG_EXAMPLE.MENU_CATEGORY },
-    {  name: locale.COMMON.TAGS, to: '/tag', show: CONFIG_EXAMPLE.MENU_TAG }
+    { name: locale.NAV.SEARCH, to: '/search', show: CONFIG_EXAMPLE.MENU_SEARCH },
+    { name: locale.NAV.ARCHIVE, to: '/archive', show: CONFIG_EXAMPLE.MENU_ARCHIVE },
+    { name: locale.COMMON.CATEGORY, to: '/category', show: CONFIG_EXAMPLE.MENU_CATEGORY }
   ]
 
   if (customNav) {
@@ -33,15 +32,18 @@ export const Nav = (props) => {
   }
 
   return (
-            <nav className="w-full bg-white md:pt-0 px-6 relative z-20  dark:bg-black">
-              <div className="container mx-auto max-w-4xl md:flex justify-between items-center text-sm md:text-md md:justify-start">
-                <ul className="w-full text-center md:text-left flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 md:items-center">
-                  {links.map(link => <MenuItemDrop key={link?.id} link={link} />)}
-                </ul>
-                {/* <div className="w-full md:w-1/3 text-center md:text-right"> */}
-                  {/* <!-- extra links --> */}
-                {/* </div> */}
-              </div>
-            </nav>
+    <nav className="w-full bg-white md:pt-0 px-6 relative z-20 dark:bg-black">
+      <div className="container mx-auto max-w-4xl md:flex justify-between items-center text-lg md:text-xl md:justify-start">
+        <ul className="w-full text-center md:text-left flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 md:items-center">
+          {links.map((link) => (
+            link.show && (
+              <MenuItemDrop key={link.id} link={link}>
+                {link.name}
+              </MenuItemDrop>
+            )
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
