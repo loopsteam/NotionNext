456a9d42c1220464fa7a1dde0902efb547ecbff8
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 01:53:57 +0800

    修改归档界面

--- themes/examplepro/LayoutArchive.js
+++ themes/examplepro/LayoutArchive.js
@@ -10,28 +10,28 @@ export const LayoutArchive = props => {
       <div className="mb-10 pb-20 md:py-12 p-3  min-h-screen w-full">
         {Object.keys(archivePosts).map((archiveTitle, index) => (
           <div key={archiveTitle}>
-            <div className={`my-8 border-b-3 border-gray-300`} ></div>
-            <ul>
+            <div className={`mt-8 border-t-2 border-gray-300`} />
+            <ul className="my-8">
               {archivePosts[archiveTitle].map(post => (
                 <li
                   key={post.id}
-                  className="p-1 text-sm md:text-base hover:scale-x-105 transform duration-500"
+                  className="border-l-2 p-1 text-sm md:text-base hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
                 >
                   <div className="flex justify-between items-center">
                     <span className="text-gray-400">
                       {post.date?.start_date}
-                    </span>{' '}
+                    </span>
                     <span className="mx-2">
                       ✨
                     </span>
-                    <Link
-                      href={`${BLOG.SUB_PATH}/${post.slug}`}
-                      passHref
-                      className="dark:text-gray-400  dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600">
-                      <a className="text-left">
+                    <div className="text-gray-600">
+                      <Link
+                        href={`${BLOG.SUB_PATH}/${post.slug}`}
+                        passHref
+                        className="dark:text-gray-400  dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer">
                         {post.title}
-                      </a>
-                    </Link>
+                      </Link>
+                    </div>
                   </div>
                 </li>
               ))}
