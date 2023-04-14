39e3bb87a88e01ac6ea87da9f09ea68ed1e2a545
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 02:42:24 +0800

    修改归档

--- themes/examplepro/LayoutArchive.js
+++ themes/examplepro/LayoutArchive.js
@@ -7,34 +7,29 @@ export const LayoutArchive = props => {
 
   return (
     <LayoutBase {...props}>
-      <div className="mb-10 pb-20 md:py-12 p-3 min-h-screen w-full">
+      <div className="mb-10 pb-20 md:py-12 p-3  min-h-screen w-full">
         {Object.keys(archivePosts).map(archiveTitle => (
           <div key={archiveTitle}>
-            <div id={archiveTitle} className="pt-16 pb-4 text-3xl dark:text-gray-300">
+            <div id={archiveTitle} className="pt-16 pb-4 text-3xl dark:text-gray-300" >
               {archiveTitle}
             </div>
             <ul>
               {archivePosts[archiveTitle].map(post => (
                 <li
                   key={post.id}
-                  className="border-l-2 p-1 text-sm md:text-base items-center hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
+                  className="border-l-2 p-1 text-sm md:text-base items-center  hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500"
                 >
-                  <div className="flex items-center">
-                    <div className="font-bold text-lg w-16 text-center">
-                      {post.date?.start_date.split('-')[0]}
-                    </div>
-                    <div className="text-gray-400 mx-2">✨</div>
-                    <div>
-                      <Link
-                        href={`${BLOG.SUB_PATH}/${post.slug}`}
-                        passHref
-                        className="dark:text-gray-400 dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600"
-                      >
-                        <div className="font-bold text-lg text-center">
-                          {post.title}
-                        </div>
-                      </Link>
-                    </div>
+                  <div id={post?.date?.start_date} className="text-lg font-bold text-center">
+                    {`${new Date(post?.date?.start_date).getFullYear()} · ${String(new Date(post?.date?.start_date).getMonth() + 1).padStart(2, '0')}`}
+                    <span className="mx-2">✨</span>
+                    <Link
+                      href={`${BLOG.SUB_PATH}/${post.slug}`}
+                      passHref
+                      className="dark:text-gray-400  dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600">
+
+                      {post.title}
+
+                    </Link>
                   </div>
                 </li>
               ))}
