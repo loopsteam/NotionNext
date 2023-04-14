eff95ba7da2ec1bb894fc7423d1bb5f797367cd3
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 06:59:22 +0800

    修改头

--- themes/examplepro/components/Header.js
+++ themes/examplepro/components/Header.js
@@ -8,23 +8,18 @@ export const Header = (props) => {
   const { siteInfo } = props
 
   return (
-      <header className="w-full px-6 bg-black  text-white relative z-10">
-            <div className="container mx-auto max-w-4xl md:flex justify-between items-center">
-                <Link
-                    href='/'
-                    className="py-6 w-full text-left md:text-left md:w-auto no-underline flex justify-center items-center">
-                    
-                    <span className="text-3xl relative">
-                      Conner Stars⭐
-                      <span className="absolute bottom-0 left-0 w-full h-1 bg-white transform transition-transform origin-left hover:scale-x-100"></span>
-                    </span>
-                    <span className="text-lg font-medium ml-2">哈喽呀</span>
-                    
-                </Link>
-                <div className="w-full md:w-auto text-center md:text-right">
-                    {/* 右侧文字 */}
-                </div>
-            </div>
-        </header>
+    <header className="w-full px-4 bg-black text-white relative z-10">
+      <div className="container mx-auto max-w-4xl md:flex justify-between items-center">
+        <div className="flex items-center">
+          <Link href="/" className="py-4 no-underline flex items-center">
+            <span className="text-2xl mr-2">Conner Stars⭐</span>
+            <span className="text-sm font-medium text-left">哈喽呀</span>
+          </Link>
+        </div>
+        <div className="w-full md:w-auto text-center md:text-right">
+          {/* 右侧文字 */}
+        </div>
+      </div>
+    </header>
   )
 }
