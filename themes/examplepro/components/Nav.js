379731b0316fa7cfca3aa7762a4e6b4dc250934c
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 17:17:30 +0800

    增加悬停阴影

--- themes/examplepro/components/Nav.js
+++ themes/examplepro/components/Nav.js
@@ -13,13 +13,20 @@ export const Nav = (props) => {
   const { locale } = useGlobal()
 
   let links = [
-    { name: '玄关.', to: 'https://connerblog.top', show: true },
-    { name: '探寻.', to: '/search', show: CONFIG_EXAMPLE.MENU_SEARCH },
-    { name: '存籍.', to: '/archive', show: CONFIG_EXAMPLE.MENU_ARCHIVE },
-    { name: '篇目.', to: '/category', show: CONFIG_EXAMPLE.MENU_CATEGORY },
-    { name: '关于.', to: 'https://connerblog.top/article/aboutme', show: true }
+    { name: '探寻', to: '/search', show: CONFIG_EXAMPLE.MENU_SEARCH },
+    { name: '存籍', to: '/archive', show: CONFIG_EXAMPLE.MENU_ARCHIVE },
+    { name: '篇目', to: '/category', show: CONFIG_EXAMPLE.MENU_CATEGORY },
+    { name: '关于', to: 'https://connerblog.top/article/aboutme', show: true }
   ]
 
+  const handleHover = (event) => {
+    event.target.style.textShadow = '1px 1px 3px rgba(0,0,0,0.5)';
+  }
+
+  const handleLeave = (event) => {
+    event.target.style.textShadow = '';
+  }
+  
   if (customNav) {
     links = links.concat(customNav)
   }
