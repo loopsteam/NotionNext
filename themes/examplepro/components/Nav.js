cf1a1163382a8afa7fa3eb9ea8dda666898df6b6
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 22:33:27 +0800

    更改导航项名称

--- themes/examplepro/components/Nav.js
+++ themes/examplepro/components/Nav.js
@@ -13,10 +13,10 @@ export const Nav = (props) => {
   const { locale } = useGlobal()
 
   let links = [
-    { name: '探寻', to: '/search', show: CONFIG_EXAMPLE.MENU_SEARCH },
-    { name: '存籍', to: '/archive', show: CONFIG_EXAMPLE.MENU_ARCHIVE },
-    { name: '篇目', to: '/category', show: CONFIG_EXAMPLE.MENU_CATEGORY },
-    { name: '关于', to: 'https://connerblog.top/article/aboutme', show: true }
+    { name: '探寻.', to: '/search', show: CONFIG_EXAMPLE.MENU_SEARCH },
+    { name: '存籍.', to: '/archive', show: CONFIG_EXAMPLE.MENU_ARCHIVE },
+    { name: '篇目.', to: '/category', show: CONFIG_EXAMPLE.MENU_CATEGORY },
+    { name: '关于.', to: 'https://connerblog.top/article/aboutme', show: true }
   ]
 
   
