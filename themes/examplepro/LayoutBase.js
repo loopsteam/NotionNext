0fe6b0ca4cfb455c661b4678e0ac666c0d9169bb
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 16:12:17 +0800

    修改下边距

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -38,7 +38,7 @@ const LayoutBase = props => {
       {/* 主体 */}
       <div style={{ marginTop: '50px', backgroundColor: '#E3EDCD', minHeight: 'calc(100vh - 50px)', height: 'auto', paddingBottom: '50px', boxSizing: 'border-box', width: '100%' }}>
       <div className="container mx-auto max-w-4xl md:flex justify-between justify-start w-full text-center md:text-left" style={{ display: 'flex', justifyContent: 'center', padding: '0 0px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', borderRadius: '10px', border: '2px solid #A16B42', width: '100%' }}>
-          <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '10px', padding: '30px 20px', margin: '0' }}>
+          <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '10px', padding: '30px 20px 10px', margin: '0' }}>
           {onLoading ? LoadingCover : children}
           </div>
       </div>
