c7de10130d4ab5ff78ce68317f124bee6df0e511
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 08:13:16 +0800

    Update LayoutBase.js

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -36,7 +36,7 @@ const LayoutBase = props => {
 
       {/* 主体 */}
       <div style={{ backgroundColor: '#E3EDCD', minHeight: 'calc(100vh - 50px)', height: 'auto', paddingBottom: '50px' }}>
-        <div className="container mx-auto max-w-4xl md:flex justify-between justify-start w-full text-center md:text-left" style={{ display: 'flex', justifyContent: 'center', padding: '0 10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', borderRadius: '10px', border: '2px solid #A16B42' }}>
+        <div className="container mx-auto max-w-4xl md:flex justify-between justify-start w-full text-center md:text-left" style={{ display: 'flex', justifyContent: 'center', padding: '0 0px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', borderRadius: '10px', border: '2px solid #A16B42' }}>
           <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '0px', padding: '0' }}>
             {onLoading ? LoadingCover : children}
           </div>
