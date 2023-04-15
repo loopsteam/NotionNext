ee13d4402ed044b2534443b8bd255f40b8868f88
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 09:08:03 +0800

    修改链接

--- themes/examplepro/components/BlogListPage.js
+++ themes/examplepro/components/BlogListPage.js
@@ -19,37 +19,40 @@ export const BlogListPage = props => {
   return (
     <div className="flex-grow md:pr-12 mb-12">
       {posts?.map(p => (
-        <article
-          key={p.id}
-          className={`mb-12 cursor-pointer transition-all duration-300 ${
-            selectedPost === p.id ? 'shadow-lg' : ''
-          }`}
-          onMouseEnter={() => setSelectedPost(p.id)}
-          onMouseLeave={() => setSelectedPost(null)}
-        >
-          <h2 className="mb-4">
-            <div className="flex flex-row items-center justify-start mb-4 text-base text-gray-700 dark:text-gray-300">
-              <span className="mr-2 text-gray-700 dark:text-gray-300">💫</span>
-              <span className="mr-2 text-gray-700 dark:text-gray-300">{p.date?.start_date || p.createdTime}</span>
-              <Link href={`/${p.slug}`} passHref>
-                <a className="text-black dark:text-gray-100 mr-2 transition-all duration-200 hover:border-gray-500 no-underline hover:no-underline">{p.title}</a>
-              </Link>
-            </div>
-          </h2>
-          {/* 搜索结果 */}
-          {p.results && (
-            <p className="p-4-lines mt-4 text-gray-700 dark:text-gray-300 text-base leading-7">
-              {p.results.map(r => (
-                <span key={r}>{r}</span>
-              ))}
-            </p>
-          )}
-        </article>
+        <Link key={p.id} href={`/${p.slug}`} passHref>
+          <a
+            className={`block mb-12 cursor-pointer transition-all duration-300 ${
+              selectedPost === p.id ? 'shadow-lg' : ''
+            }`}
+            onMouseEnter={() => setSelectedPost(p.id)}
+            onMouseLeave={() => setSelectedPost(null)}
+          >
+            <h2 className="mb-4">
+              <div className="flex flex-row items-center justify-start mb-4 text-base text-gray-700 dark:text-gray-300">
+                <span className="mr-2 text-gray-700 dark:text-gray-300">{p.date?.start_date || p.createdTime}</span>
+                <span className="mr-2 text-gray-700 dark:text-gray-300">💫</span>
+                <span className="mr-2 text-gray-700 dark:text-gray-300">{p.title}</span>
+              </div>
+            </h2>
+            {/* 搜索结果 */}
+            {p.results && (
+              <p className="p-4-lines mt-4 text-gray-700 dark:text-gray-300 text-base leading-7">
+                {p.results.map(r => (
+                  <span key={r}>{r}</span>
+                ))}
+              </p>
+            )}
+          </a>
+        </Link>
       ))}
       <div className="flex justify-between text-base">
         <Link
-          href={{ pathname: currentPage - 1 === 1 ? `${pagePrefix}/` : `${pagePrefix}/page/${currentPage - 1}`, query: router.query.s ? { s: router.query.s } : {} }}
-          className={`${showPrev ? 'bg-black ' : 'bg-gray pointer-events-none '} text-white no-underline py-2 px-3 rounded`}>
+          href={{
+            pathname: currentPage - 1 === 1 ? `${pagePrefix}/` : `${pagePrefix}/page/${currentPage - 1}`,
+            query: router.query.s ? { s: router.query.s } : {},
+          }}
+          className={`${showPrev ? 'bg-black ' : 'bg-gray pointer-events-none '} text-white no-underline py-2 px-3 rounded`}
+        >
           {locale.PAGINATION.PREV}
         </Link>
         <Link
