7a4765729855c61688cba99394be244d9944eb37
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 08:23:28 +0800

    修改圆角

--- themes/examplepro/LayoutBase.js
+++ themes/examplepro/LayoutBase.js
@@ -35,16 +35,15 @@ const LayoutBase = props => {
       </div>
 
       {/* 主体 */}
-      <div style={{ marginTop: '50px',backgroundColor: '#E3EDCD', minHeight: 'calc(100vh - 50px)', height: 'auto', paddingBottom: '50px' }}>
-        <div className="container mx-auto max-w-4xl md:flex justify-between justify-start w-full text-center md:text-left" style={{ display: 'flex', justifyContent: 'center', padding: '0 0px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', borderRadius: '10px', border: '2px solid #A16B42' }}>
-          <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '0px', padding: '0' }}>
-            <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '0px', padding: '0' }}>
+      <div style={{ marginTop: '50px', backgroundColor: '#E3EDCD', minHeight: 'calc(100vh - 50px)', height: 'auto', paddingBottom: '50px' }}>
+        <div className="container mx-auto max-w-4xl md:flex justify-between justify-start w-full text-center md:text-left" style={{ display: 'flex', justifyContent: 'center', padding: '0 20px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', borderRadius: '10px', border: '2px solid #A16B42' }}>
+          <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '10px', padding: '30px 20px' }}>
+            <div className="flex-grow" style={{ backgroundColor: '#FFFFFF', boxShadow: '0 0 0px rgba(0, 0, 0, 0.2)', borderRadius: '10px', padding: '30px 20px' }}>
               {onLoading ? LoadingCover : children}
             </div>
           </div>
         </div>
       </div>
-
       <Footer {...props} />
 
       <div className='fixed right-4 bottom-4 z-10'>
