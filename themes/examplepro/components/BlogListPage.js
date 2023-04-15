4354d8261ccbb3f0a7d8b72519b2406226179191
宋昆明 <50096506+loopsteam@users.noreply.github.com>
2023-04-15 08:29:23 +0800

    修改加粗字体

--- themes/examplepro/components/BlogListPage.js
+++ themes/examplepro/components/BlogListPage.js
@@ -22,13 +22,13 @@ export const BlogListPage = props => {
         <article
           key={p.id}
           className={`mb-12 cursor-pointer transition-all duration-300 ${
-            selectedPost === p.id ? 'bg-gray-100' : ''
+            selectedPost === p.id ? '' : 'shadow-lg'
           }`}
           onMouseEnter={() => setSelectedPost(p.id)}
           onMouseLeave={() => setSelectedPost(null)}
         >
           <h2 className="mb-4">
-            <div className="flex flex-row items-center justify-start mb-4 text-sm font-bold text-gray-700 dark:text-gray-300">
+            <div className="flex flex-row items-center justify-start mb-4 text-base text-gray-700 dark:text-gray-300">
               <span className="mr-2 text-gray-700 dark:text-gray-300">{/*小圆点*/}💫</span>
               <span className="mr-2">{p.date?.start_date || p.createdTime}</span>
               <span className="text-right">{/*去掉原来的"."*/}</span>
@@ -42,7 +42,7 @@ export const BlogListPage = props => {
           </h2>
           {/* 搜索结果 */}
           {p.results && (
-            <p className="p-4-lines mt-4 text-gray-700 dark:text-gray-300 text-sm font-light leading-7">
+            <p className="p-4-lines mt-4 text-gray-700 dark:text-gray-300 text-base leading-7">
               {p.results.map(r => (
                 <span key={r}>{r}</span>
               ))}
@@ -50,7 +50,7 @@ export const BlogListPage = props => {
           )}
         </article>
       ))}
-      <div className="flex justify-between text-sm">
+      <div className="flex justify-between text-base">
         <Link
           href={{ pathname: currentPage - 1 === 1 ? `${pagePrefix}/` : `${pagePrefix}/page/${currentPage - 1}`, query: router.query.s ? { s: router.query.s } : {} }}
           className={`${showPrev ? 'bg-black ' : 'bg-gray pointer-events-none '} text-white no-underline py-2 px-3 rounded`}>
